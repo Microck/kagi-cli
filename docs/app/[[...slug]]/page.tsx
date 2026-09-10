348d0d892bff0bc4e4ddd82c033df4e1592c9a94
Microck <contact@micr.dev>
2026-09-10 09:16:16 +0000

    fix(docs): apply Fumadocs MDX components

--- docs/app/[[...slug]]/page.tsx
+++ docs/app/[[...slug]]/page.tsx
@@ -27,7 +27,7 @@ export default async function Page(props: {
     >
       <DocsPage toc={page.data.toc}>
         <DocsBody>
-          <MDX />
+          <MDX components={getMDXComponents()} />
         </DocsBody>
       </DocsPage>
     </DocsLayout>
